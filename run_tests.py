4e0ef52e55e5e87dfb0898bec68476938198c8d3
chris <christoph-heck@web.de>
2025-05-15 10:07:38 +0200

    update

--- run_tests.py
+++ run_tests.py
@@ -8,18 +8,18 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 header = [
     "threshold", 
-    "true_positive_10", "false_positive_10", "true_negative_10", "false_negative_10",
-    "true_positive_11", "false_positive_11", "true_negative_11", "false_negative_11",
-    "true_positive_12", "false_positive_12", "true_negative_12", "false_negative_12",
-    "true_positive_13", "false_positive_13", "true_negative_13", "false_negative_13",
+    # "true_positive_10", "false_positive_10", "true_negative_10", "false_negative_10",
+    # "true_positive_11", "false_positive_11", "true_negative_11", "false_negative_11",
+    # "true_positive_12", "false_positive_12", "true_negative_12", "false_negative_12",
+    # "true_positive_13", "false_positive_13", "true_negative_13", "false_negative_13",
     "true_positive_14", "false_positive_14", "true_negative_14", "false_negative_14"
 ]
 
 data_dirs = [
-    "/home/chris/experiment_data/10_2025_02_20-2025_02_27/classification",
-    "/home/chris/experiment_data/11_2025_02_27-2025_03_04/classification",
-    "/home/chris/experiment_data/12_2025_03_06-2025_03_10/classification",
-    "/home/chris/experiment_data/13_2025_03_11-2025_03_14/classification",
+    #"/home/chris/experiment_data/10_2025_02_20-2025_02_27/classification",
+    #"/home/chris/experiment_data/11_2025_02_27-2025_03_04/classification",
+    #"/home/chris/experiment_data/12_2025_03_06-2025_03_10/classification",
+    #"/home/chris/experiment_data/13_2025_03_11-2025_03_14/classification",
     "/home/chris/experiment_data/14_2025_03_14-2025_03_21/classification"
 ]
 
@@ -31,7 +31,7 @@ normalization_dirs = [
     "/home/chris/online-classification-simulation/FCN_temperature/adjusted-min-max/model_4",
 ]
 
-validation_methods = ["max", "both", "mean"]
+validation_methods = ["max", "min", "mean"]
 
 prefixes = ["C1", "C2"]
 
@@ -60,45 +60,45 @@ for prefix in [prefixes[0]]:
                 # Write header to CSV file.
                 writer.writerow(header)
 
-                    # For the first data_dir (labeled with "10"), always use prefix "C1"
-                tp10, fp10, tn10, fn10 = main.main(
-                        data_dir=data_dirs[0],
-                        classifier_dir=normalization_dir,
-                        normalization=normalization,
-                        prefix="C1",  # data_dirs[0] does not have a prefix C2
-                        threshold=threshold,
-                        objective=objective,
-                        validation_method=validation_method
-                    )
+                #     # For the first data_dir (labeled with "10"), always use prefix "C1"
+                # tp10, fp10, tn10, fn10 = main.main(
+                #         data_dir=data_dirs[0],
+                #         classifier_dir=normalization_dir,
+                #         normalization=normalization,
+                #         prefix="C1",  # data_dirs[0] does not have a prefix C2
+                #         threshold=threshold,
+                #         objective=objective,
+                #         validation_method=validation_method
+                #     )
 
-                    # For the remaining data_dirs, use the current prefix value.
-                tp11, fp11, tn11, fn11 = main.main(
-                        data_dir=data_dirs[1],
-                        classifier_dir=normalization_dir,
-                        normalization=normalization,
-                        prefix=prefix,
-                        threshold=threshold,
-                        objective=objective,
-                        validation_method=validation_method
-                    )
-                tp12, fp12, tn12, fn12 = main.main(
-                        data_dir=data_dirs[2],
-                        classifier_dir=normalization_dir,
-                        normalization=normalization,
-                        prefix=prefix,
-                        threshold=threshold,
-                        objective=objective,
-                        validation_method=validation_method
-                    )
-                tp13, fp13, tn13, fn13 = main.main(
-                        data_dir=data_dirs[3],
-                        classifier_dir=normalization_dir,
-                        normalization=normalization,
-                        prefix=prefix,
-                        threshold=threshold,
-                        objective=objective,
-                        validation_method=validation_method
-                    )
+                #     # For the remaining data_dirs, use the current prefix value.
+                # tp11, fp11, tn11, fn11 = main.main(
+                #         data_dir=data_dirs[1],
+                #         classifier_dir=normalization_dir,
+                #         normalization=normalization,
+                #         prefix=prefix,
+                #         threshold=threshold,
+                #         objective=objective,
+                #         validation_method=validation_method
+                #     )
+                # tp12, fp12, tn12, fn12 = main.main(
+                #         data_dir=data_dirs[2],
+                #         classifier_dir=normalization_dir,
+                #         normalization=normalization,
+                #         prefix=prefix,
+                #         threshold=threshold,
+                #         objective=objective,
+                #         validation_method=validation_method
+                #     )
+                # tp13, fp13, tn13, fn13 = main.main(
+                #         data_dir=data_dirs[3],
+                #         classifier_dir=normalization_dir,
+                #         normalization=normalization,
+                #         prefix=prefix,
+                #         threshold=threshold,
+                #         objective=objective,
+                #         validation_method=validation_method
+                #     )
                 tp14, fp14, tn14, fn14 = main.main(
                         data_dir=data_dirs[4],
                         classifier_dir=normalization_dir,
@@ -112,10 +112,10 @@ for prefix in [prefixes[0]]:
                     # Create the row of data to be written.
                 row = [
                         threshold,
-                        tp10, fp10, tn10, fn10,
-                        tp11, fp11, tn11, fn11,
-                        tp12, fp12, tn12, fn12,
-                        tp13, fp13, tn13, fn13,
+                        # tp10, fp10, tn10, fn10,
+                        # tp11, fp11, tn11, fn11,
+                        # tp12, fp12, tn12, fn12,
+                        # tp13, fp13, tn13, fn13,
                         tp14, fp14, tn14, fn14
                     ]
                 writer.writerow(row)
